b7acb7f09e810a1f3847368421dd28b4f45924b2
Pekka Sinkkonen <peksin@protonmail.com>
2020-08-05 14:21:10 +0300

    baseUrl muutos

---
+++ build/precache-manifest.0c405394194b90200044e3105ed2c85d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f17b3a6db3ad562f0966eff0bc7a28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee79b3e4c8cd9a5c3ec5",
+    "url": "/static/css/main.30abf71c.chunk.css"
+  },
+  {
+    "revision": "ef61acb61f774e07c1a8",
+    "url": "/static/js/2.5928e83f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5928e83f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee79b3e4c8cd9a5c3ec5",
+    "url": "/static/js/main.5b6c3ea2.chunk.js"
+  },
+  {
+    "revision": "1f882ea84ee44af843ea",
+    "url": "/static/js/runtime-main.6e81a402.js"
+  }
+]);
