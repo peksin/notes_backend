5c08964331ec14de69f9611815b793408cb35e96
Pekka Sinkkonen <peksin@protonmail.com>
2020-08-06 13:43:29 +0300

    typon korjaus

--- models/note.js
+++ models/note.js
@@ -19,7 +19,7 @@ const noteSchema = new mongoose.Schema({
 })
 
 noteSchema.set('toJSON', {
-    transfrom: (document, returnedObject) => {
+    transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
