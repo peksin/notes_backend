92bf9c7413f79068037477c5f5e60dd5f20de987
Pekka Sinkkonen <peksin@protonmail.com>
2020-08-06 13:35:31 +0300

    mongodb toimii backin ja frontin kanssa

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const { response } = require('express')
 const cors = require('cors')
 const app = express()
-
+const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
     console.log(`Method:`, request.method)
@@ -59,22 +60,22 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
     const body = request.body
 
-    if (!body.content) {
+    if (body.content === undefined) {
         // 400 bad request
         return response.status(400).json({
             error: 'content missing'
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId(),
-    }
+    })
 
-    notes = notes.concat(note)
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 app.get('/', (req, res) => {
@@ -82,18 +83,15 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/notes', (req, res) => {
-    res.json(notes)
+    Note.find({}).then(notes => {
+        res.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -108,7 +106,7 @@ app.delete('/api/notes/:id', (request, response) => {
 app.use(unknownEndpoint)
 
 // Palvelin kayntiin
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
 })
