5ec656316abb07232c4247f1e0c79c35cb85a93b
Pekka Sinkkonen <peksin@protonmail.com>
2020-08-07 23:25:09 +0300

    osa3 lint esimerkki

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-require('dotenv').config()
-const express = require('express')
-const { response } = require('express')
-const cors = require('cors')
+require("dotenv").config()
+const express = require("express")
+const { response } = require("express")
+const cors = require("cors")
 const app = express()
-const Note = require('./models/note')
-const { update } = require('./models/note')
+const Note = require("./models/note")
+const { update } = require("./models/note")
 
 const requestLogger = (request, response, next) => {
-    console.log(`Method:`, request.method)
-    console.log(`Path:  `, request.path)
-    console.log(`Body:  `, request.body)
-    console.log(`----------`)
+    console.log("Method:", request.method)
+    console.log("Path:  ", request.path)
+    console.log("Body:  ", request.body)
+    console.log("----------")
     next()
 }
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({error: 'unknown endpoint'})
+    response.status(404).send({error: "unknown endpoint"})
 }
 
 // middlewaret kayttoon
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
-app.use(express.static('build'))
+app.use(express.static("build"))
 
 let notes = [
     {
-      id: 1,
-      content: "HTML is easy",
-      date: "2020-01-10T17:30:31.098Z",
-      important: true
+        id: 1,
+        content: "HTML is easy",
+        date: "2020-01-10T17:30:31.098Z",
+        important: true
     },
     {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2020-01-10T18:39:34.091Z",
-      important: false
+        id: 2,
+        content: "Browser can execute only Javascript",
+        date: "2020-01-10T18:39:34.091Z",
+        important: false
     },
     {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2020-01-10T19:20:14.298Z",
-      important: true
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2020-01-10T19:20:14.298Z",
+        important: true
     }
 ]
 
@@ -58,14 +58,14 @@ const generateId = () => {
  * Routet tanne
  *=========================*/
 
- // add one note
-app.post('/api/notes', (request, response, next) => {
+// add one note
+app.post("/api/notes", (request, response, next) => {
     const body = request.body
 
     if (body.content === undefined) {
         // 400 bad request
         return response.status(400).json({
-            error: 'content missing'
+            error: "content missing"
         })
     }
 
@@ -85,19 +85,19 @@ app.post('/api/notes', (request, response, next) => {
 })
 
 // get a useless hello world
-app.get('/', (req, res) => {
-    res.send('<h1>Hello Worldi!</h1>')
+app.get("/", (req, res) => {
+    res.send("<h1>Hello Worldi!</h1>")
 })
 
 // get all
-app.get('/api/notes', (req, res) => {
+app.get("/api/notes", (req, res) => {
     Note.find({}).then(notes => {
         res.json(notes)
     })
 })
 
 // get one note
-app.get('/api/notes/:id', (request, response) => {
+app.get("/api/notes/:id", (request, response, next) => {
     Note.findById(request.params.id)
         .then(note => {
             if (note) {
@@ -110,7 +110,7 @@ app.get('/api/notes/:id', (request, response) => {
 })
 
 // delete one note
-app.delete('/api/notes/:id', (request, response, next) => {
+app.delete("/api/notes/:id", (request, response, next) => {
     Note.findByIdAndDelete(request.params.id)
         .then(result => {
             response.status(204).end()
@@ -119,7 +119,7 @@ app.delete('/api/notes/:id', (request, response, next) => {
 })
 
 // update one note
-app.put('/api/notes/:id', (req, res, next) => {
+app.put("/api/notes/:id", (req, res, next) => {
     const body = req.body
 
     const note = {
@@ -145,9 +145,9 @@ app.use(unknownEndpoint)
 const errorHandler = (error, req, res, next) => {
     console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return res.status(400).send({error: 'malformatted id'})
-    } else if (error.name === 'ValidationError') {
+    if (error.name === "CastError") {
+        return res.status(400).send({error: "malformatted id"})
+    } else if (error.name === "ValidationError") {
         return res.status(400).json({error: error.message})
     }
 
